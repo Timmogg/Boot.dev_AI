b6c4826e0bdfeb7cc56099dc05df274c15448939
Timmogg <timothymcpheron8@gmail.com>
2025-10-16 19:46:00 -0400

    implement function declaration for AI

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     abs_working = os.path.abspath(working_directory)
@@ -17,4 +18,20 @@ def get_files_info(working_directory, directory="."):
             lines.append(f"- {name}: file_size={file_size} bytes, is_dir={is_dir}")
         return "\n".join(lines)
     except OSError as e:
-        return f"Error: {e}"    
+        return f"Error: {e}"
+
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,6 +12,20 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     verbose = "--verbose" in sys.argv
     user_prompt = next((arg for arg in sys.argv[1:] if not arg.startswith('--')), None)
     if user_prompt == None:
@@ -20,11 +36,18 @@ def main():
     response = client.models.generate_content(
             model = "gemini-2.0-flash-001",
             contents = messages,
+            config=types.GenerateContentConfig(
+                tools=[available_functions], system_instruction=system_prompt
+            )
         )
     if verbose:
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text) 
+    if response.function_calls:
+        for fc in response.function_calls:
+            print(f"Calling function: {fc.name}({fc.args})")
+    else:
+        print(response.text) 
 if __name__ == "__main__":
     main()
